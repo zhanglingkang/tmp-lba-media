a6f29de3882fb370662baed1831ce77ec8bd1ab4
zhanglingkang <lingkang.zlk@alibaba-inc.com>
2015-12-15 19:38:09 +0800

    修改构建脚本

--- bin/build.sh
+++ bin/build.sh
@@ -5,29 +5,34 @@ cd "$__DIRNAME__"
 
 ./init-env.sh
 
-cd ../;tnpm i
-
-#由于aone上下载依赖包太慢，暂时使用在发布前在本地构建的方式
-#cd statics;tnpm i;gulp -p;
-
 envType=$1
 __ENV__=daily
 case $envType in
     daily )
     #日常环境
     __ENV__=daily
+    sudo tnpm update -g
     ;;
     prepub )
     #预发环境pre
     __ENV__=production
+    sudo tnpm update -g
     ;;
     publish )
     __ENV__=production
+    sudo tnpm update -g
     ;;
     * )
     #项目环境
     ;;
 esac
+
+cd ../;tnpm i
+
+#由于aone上下载依赖包太慢，暂时使用在发布前在本地构建的方式
+cd statics;tnpm i;gulp -p;
+
+
 
 cd "$__DIRNAME__";cd ../app/config
 cp config_${__ENV__}.js config.js
