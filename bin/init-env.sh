a5757d64fba09a6f913d18e6a3df2fa8d64752f3
zhanglingkang <lingkang.zlk@alibaba-inc.com>
2015-12-15 20:07:09 +0800

    去掉init-env中的sudo

--- bin/init-env.sh
+++ bin/init-env.sh
@@ -2,12 +2,12 @@
 if hash tnpm 2>/dev/null; then
    echo "tnpm installed"
 else
-   sudo npm i -g tnpm --registry=http://registry.npm.alibaba-inc.com
+    npm i -g tnpm --registry=http://registry.npm.alibaba-inc.com
 fi
 if hash gulp 2>/dev/null; then
    echo "gulp installed"
 else
-   sudo tnpm i gulp -g
+    tnpm i gulp -g
 fi
 
 
